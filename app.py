54a737fe32a128470372b14eda17764fee80ae64
xkaledx <84477861+xkaledx@users.noreply.github.com>
2021-05-22 21:21:49 -0600

    Update app.py

--- app.py
+++ app.py
@@ -27,42 +27,42 @@ st.write('Esta es una aplicación web para clasificar la calidad de su vino en f
          valor de cada característica. Después de eso, haga clic en el botón Predecir en la parte inferior para\
          ver la predicción del clasificador.')
 
-fixed_acidity = st.sidebar.slider(label = 'acidez fija', min_value = 4.0,
+fixed_acidity = st.sidebar.slider(label = 'Acidez fija', min_value = 4.0,
                           max_value = 16.0 ,
                           value = 10.0,
                           step = 0.1)
 
-volatile_acidity = st.sidebar.slider(label = 'acidez volátil', min_value = 0.00,
+volatile_acidity = st.sidebar.slider(label = 'Acidez volátil', min_value = 0.00,
                           max_value = 2.00 ,
                           value = 1.00,
                           step = 0.01)
                           
-citric_acid = st.sidebar.slider(label = 'ácido cítrico', min_value = 0.00,
+citric_acid = st.sidebar.slider(label = 'Ácido cítrico', min_value = 0.00,
                           max_value = 1.00 ,
                           value = 0.50,
                           step = 0.01)                          
 
-residual_sugar = st.sidebar.slider(label = 'azúcares residuales', min_value = 0.0,
+residual_sugar = st.sidebar.slider(label = 'Azúcares residuales', min_value = 0.0,
                           max_value = 16.0 ,
                           value = 8.0,
                           step = 0.1)
 
-chlorides = st.sidebar.slider(label = 'cloruros', min_value = 0.000,
+chlorides = st.sidebar.slider(label = 'Cloruros', min_value = 0.000,
                           max_value = 1.000 ,
                           value = 0.500,
                           step = 0.001)
    
-f_sulf_diox = st.sidebar.slider(label = 'dióxido de azufre libre', min_value = 1,
+f_sulf_diox = st.sidebar.slider(label = 'Dióxido de azufre libre', min_value = 1,
                           max_value = 72,
                           value = 36,
                           step = 1)
 
-t_sulf_diox = st.sidebar.slider(label = 'dióxido de azufre total', min_value = 6,
+t_sulf_diox = st.sidebar.slider(label = 'Dióxido de azufre total', min_value = 6,
                           max_value = 289 ,
                           value = 144,
                           step = 1)
 
-density = st.sidebar.slider(label = 'densidad	', min_value = 0.0000,
+density = st.sidebar.slider(label = 'Densidad	', min_value = 0.0000,
                           max_value = 2.0000 ,
                           value = 0.9900,
                           step = 0.0001)
@@ -72,12 +72,12 @@ ph = st.sidebar.slider(label = 'pH', min_value = 2.00,
                           value = 3.00,
                           step = 0.01)
                           
-sulphates = st.sidebar.slider(label = 'sulfatos', min_value = 0.00,
+sulphates = st.sidebar.slider(label = 'Sulfatos', min_value = 0.00,
                           max_value = 2.00,
                           value = 0.50,
                           step = 0.01)
 
-alcohol = st.sidebar.slider(label = 'alcohol', min_value = 8.0,
+alcohol = st.sidebar.slider(label = 'Alcohol', min_value = 8.0,
                           max_value = 15.0,
                           value = 10.5,
                           step = 0.1)
